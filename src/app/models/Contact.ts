c150d6eb46a3fd00ea49d3de3f52ff9efb438d2b
guillaumeBonnet <bonnet.guillaume.93@gmail.com>
2017-10-11 19:50:21 +0200

    update contact ac group et phone

--- src/app/models/Contact.ts
+++ src/app/models/Contact.ts
@@ -1,7 +1,9 @@
 import Coordonne from './Coordonne';
+import Group from './Group'
 
 export default class Contact {
     constructor(  public id:number
+                , public group:Group
                 , public nom:String
                 , public prenom:String
                 , public profil:Array<String>
