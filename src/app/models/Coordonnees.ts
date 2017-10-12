89addf8ecb7608345fd02a61f33117d178af9fac
GuillaumeBonnet <bonnet.guillaume.9.3@gmail.com>
2017-10-12 14:12:52 +0200

    Classes angular (#7)
    * init file service
    
    * Revert "init file service"
    
    This reverts commit e70a5f7b73b2b163a4f379fa20d3462eea114da8.
    
    * creation models classes avec un test basique mocha, le test passe mais je ne peux pas le lancer par commande racourcie npm
    
    * added id to each and every classes
    
    * update contact ac group et phone
    
    * cordonnee

---
+++ src/app/models/Coordonnees.ts
@@ -0,0 +1,10 @@
+export default class Coordonnees {
+    constructor(  public id:number
+                , public adresse:String
+                , public codePostal:number
+                , public ville:String
+                , public phone:String
+                , public email:String) {
+    }
+
+  }
