797a3f36582736637aaeee0dd11e0bcd10149700
georgesdaniel <30408831+georgesdaniel@users.noreply.github.com>
2017-10-17 22:08:26 +0200

    Form user create ihm (#26)
    * Form contact + form login
    
    * design form contact
    
    * Design Form Contact + Form User ok
    
    * commit pour merge
    
    * bug resolus design
    
    * rename branche & supp url inutiles
    
    * add Directory Font
    
    * Form Typescript User
    
    * test
    
    * icones oki
    
    * resolve background oki
    
    * control champs form contact ok
    
    * test
    
    * new design en place
    
    * no message
    
    * redesign de la page login
    
    * page login Oki Ouf
    
    * header DIV fix plus supression liens inutiles
    
    * link sign in fonctionne
    
    * header avec ngIf
    
    * yutr

--- src/app/services/header-interceptor.service.ts
+++ src/app/services/header-interceptor.service.ts
@@ -4,25 +4,26 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpHeaders } fro
 import { Observable } from "rxjs/Observable";
 import { apiUrl } from "./apiRequests.service";
 
+
 import {  } from "module";
 @Injectable()
 export class HeaderInterceptorService implements HttpInterceptor {
-  
-  constructor() {  
+
+  constructor() {
    }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Le traitement est déclenché si la requête sur l'API sample.com
-   
+
    if (req.url.includes(apiUrl)) {
-      // Ajout du header Content-Type 
+      // Ajout du header Content-Type
       // => Il est important de noter qu'une requête est immuable, vous devez la cloner
       if(window.localStorage.getItem("tokenAuth")) {
         let tokenHeader = new HttpHeaders({'Authorization': window.localStorage.getItem("tokenAuth")});
         const clone = req.clone({ headers : tokenHeader});
-                             
+
         return next.handle(clone);
-      }        
-      
+      }
+
     }
     return next.handle(req);
   }
